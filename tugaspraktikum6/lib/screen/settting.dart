9036d19e79516917008953129bfeeab2bb686c15
radenrishwan <radenrishwan@gmail.com>
2022-12-09 20:42:23 +0700

    doc: add about us text

--- tugaspraktikum6/lib/screen/settting.dart
+++ tugaspraktikum6/lib/screen/settting.dart
@@ -16,6 +16,7 @@ class SettingScreen extends StatelessWidget {
           StatefulBuilder(builder: (context, setState) {
             return ListTile(
               title: const Text('Dark Mode'),
+              leading: const Icon(Icons.dark_mode),
               trailing: Switch(
                 value: context.watch<ThemeModeProvider>().darkMode,
                 onChanged: (value) {
@@ -27,9 +28,41 @@ class SettingScreen extends StatelessWidget {
             );
           }),
           ListTile(
-            title: const Text('About'),
-            trailing: const Icon(Icons.arrow_forward_ios),
-            onTap: () {},
+            title: const Text('About Us'),
+            leading: const Icon(Icons.info),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('About Us'),
+                    content: const Text(
+                      '''
+Hi, Buddy 👋👋👋.
+
+Thanks for using our app. please send us star at github if this app really usefull. And we also open to all contribute, please check our github if you interested:
+
+https://github.com/radenrishwan/tugaspraktikumflutter/tree/master/tugaspraktikum6
+
+this apps created due to my assignment in college, so please dont expect too much from this app.
+
+created by : Raden Mohamad Rishwan, Reza Zulfikri
+''',
+                      textAlign: TextAlign.justify,
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
           )
         ],
       ),
